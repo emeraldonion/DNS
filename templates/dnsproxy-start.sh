3712fd0f1fca5dd75d14b748f606b7ab3424414f
Nate Sales <nate@natesales.net>
2021-02-25 20:29:33 -0800

    add TLS cert to dnsproxy-start.sh

--- templates/dnsproxy-start.sh
+++ templates/dnsproxy-start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/usr/bin/dnsproxy --upstream=127.0.0.1:5353 --bootstrap=127.0.0.1:5353 --https-port=8443 --tls=port=8053 --quic-port=8784 --cache-size=0
+/usr/bin/dnsproxy \
+  --cache-size=0 \
+  --upstream=127.0.0.1:5353 \
+  --bootstrap=127.0.0.1:5353 \
+  --https-port=8443 \
+  --tls=port=8053 \
+  --quic-port=8784 \
+  --tls-crt=/opt/eodns/certificates/cert.pem \
+  --tls-key=/opt/eodns/certificates/key.pem
